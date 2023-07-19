c4888e994abe8c7bda8ef7d7f4dd91495ea60b00
victor Macharia <Techvictor653@gmail.com>
2023-07-19 16:07:07 +0300

    crazy learning next.js, i think this is the second crazy commit

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,7 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
-}
+import { NextResponse } from "next/server"
+
+
+export async function GET() {
+    return NextResponse.json({ "message": 'Hello, Next.js!' })
+  }
+  
